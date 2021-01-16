4012fbb06872ea2b85bcabb9afef56201ff4a5ed
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-16 12:22:29 +0100

    Testing if newsletter signup is working.

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/997eab9f3e";
+
+  const options = {
+    method: "POST",
+    auth: "roybrussel:e2664e3114d69e49ab6bd4d068780713-us7",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log("TEST");
+    });
+  });
+
+  request.write(jsonData);
+  request.end;
 });
 
 app.listen(3000, function (req, res) {
